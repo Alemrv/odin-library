4f1f55e17cabf968bca9f1fef2d0dbd6aa6baa9c
Alemrv <alemrv@gmail.com>
2021-11-10 21:57:19 -0400

    added more html elements added some css styles added a modal

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const button = document.getElementById("button");
+const test = document.getElementById("test");
+var modal = document.getElementById("myModal");
+var span = document.getElementById("close");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 let myLibrary =[];
 
@@ -13,15 +23,23 @@ Book.prototype.info = function(){
 }
 
 function addBookToLibrary(){
+    modal.style.display = "block";
     let book = prompt("Enter a book name");
     if (book != null) {
         myLibrary.push(book);
         console.log(myLibrary);
     }
+    for (let index = 0; index < myLibrary.length; index++) {
+        test.innerHTML += myLibrary[index];
+    }
 }
 
 button.addEventListener("click", addBookToLibrary);
+span.addEventListener("click", closeModal);
 
+function closeModal(){
+    modal.style.display = "none";
+}
 // const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
 
 // console.log(theHobbit.info());
